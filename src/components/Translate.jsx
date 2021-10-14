9293cb4880b20d60f31360db958c03cdf62c6eed
Austin Flatt <austindflatt@gmail.com>
2021-10-14 18:50:23 -0400

    installed axios for API requests and created state for input text

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Center, Input, Textarea, Select, Button } from '@mantine/core';
 
 export const Translate = () => {
+    const [inputText, setInputText] = useState('');
+    
     return (
         <div>
             
